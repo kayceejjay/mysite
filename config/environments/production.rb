c683d6fbd487f713e2b718a02bc089cb8802c609
Kenneth Jones <kayceejjay@gmail.com>
2018-08-29 20:23:25 +0000

    Added credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,12 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+
+
+  # Rails credentials in an environment variable (Old method)
+  # config.secret_key_base = ENV['SECRET_KEY_BASE']
+
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
